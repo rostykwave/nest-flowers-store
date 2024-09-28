f4a63462601351ef1eee41c123c3462eaef0045c
Rostyslav <rostykwave@gmail.com>
2024-09-28 19:00:28 +0300

    feat: guard concept

---
+++ src/conception/auth.guard.ts
@@ -0,0 +1,17 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const isAuth = request.headers.authorization === 'secret';
+    if (!isAuth) throw new UnauthorizedException('Not authorized');
+
+    return isAuth;
+  }
+}
--- src/flowers/flowers.controller.ts
+++ src/flowers/flowers.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { FlowersService } from './flowers.service';
 import { ParseIntPipe } from 'src/conception/parseInt.pipe';
+import { AuthGuard } from 'src/conception/auth.guard';
 
 @Controller('flowers')
 export class FlowersController {
   constructor(private readonly flowersService: FlowersService) {}
 
   @Get()
+  @UseGuards(AuthGuard)
   findAll(@Query('pageNumber', ParseIntPipe) pageNumber: number) {
     console.log('pageNumber', pageNumber);
     console.log('flowers findAll controller');

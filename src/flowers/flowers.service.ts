b9f743cd6085d9ea48de6b2eb642d61f06171e20
Rostyslav <rostykwave@gmail.com>
2024-10-04 11:34:43 +0300

    feat: add and link prisma service

--- src/flowers/flowers.service.ts
+++ src/flowers/flowers.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@nestjs/common';
+// import { Flower } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class FlowersService {
+  constructor(private readonly prisma: PrismaService) {}
   findAll() {
-    return [
-      {
-        name: 'Rose',
-        color: 'Red',
-        price: 5,
-      },
-      { name: 'Lily', color: 'White', price: 6 },
-      { name: 'Tulip', color: 'Yellow', price: 7 },
-    ];
+    return this.prisma.flower.findMany();
+    // return [
+    //   {
+    //     name: 'Rose',
+    //     color: 'Red',
+    //     price: 5,
+    //   },
+    //   { name: 'Lily', color: 'White', price: 6 },
+    //   { name: 'Tulip', color: 'Yellow', price: 7 },
+    // ];
   }
 }

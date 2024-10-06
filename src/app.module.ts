b3c07899f5df3ac12d09866b6a6a712644b88ce6
Rostyslav <rostykwave@gmail.com>
2024-10-06 21:06:56 +0300

    feat: add configService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { FlowersModule } from './flowers/flowers.module';
 import { LoggerMiddleware } from './conception/middleware';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [FlowersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    FlowersModule,
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- src/flowers/flowers.service.ts
+++ src/flowers/flowers.service.ts
@@ -2,11 +2,20 @@ import { Injectable } from '@nestjs/common';
 // import { Flower } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
 import { CreateFlowersDto } from './flowers-dto/flowers.dto';
+import { ConfigService } from '@nestjs/config';
+import { EnumAppMode } from 'src/types';
 
 @Injectable()
 export class FlowersService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly configService: ConfigService,
+  ) {}
   findAll() {
+    console.log(
+      'this.configService.get("MODE")',
+      this.configService.get<EnumAppMode>('MODE'),
+    );
     return this.prisma.flower.findMany();
     // return [
     //   {

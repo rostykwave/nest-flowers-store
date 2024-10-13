97fb3d752303d94c3835b2e9110f8899fab5c4e2
Rostyslav <rostykwave@gmail.com>
2024-10-13 16:41:59 +0300

    feat: add microservice

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,10 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { FlowersModule } from './flowers/flowers.module';
 import { LoggerMiddleware } from './conception/middleware';
 import { ConfigModule } from '@nestjs/config';
+import { MicroserviceModule } from './microservice/microservice.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
@@ -9,7 +13,20 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
     FlowersModule,
+    MicroserviceModule,
+    ClientsModule.register([
+      {
+        name: 'ORDER_SERVICE',
+        transport: Transport.TCP,
+        options: {
+          host: 'localhost',
+          port: 8877,
+        },
+      },
+    ]),
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
